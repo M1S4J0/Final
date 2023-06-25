5b7d6c71de7514a139e61fbf5350eab739e8eee4
Johan PC <Johan PC@JOHAN>
2023-06-25 17:09:30 -0500

    vr. 1.2 juego interrogados

--- src/interrogados/Main.java
+++ src/interrogados/Main.java
@@ -13,7 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Juego extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
