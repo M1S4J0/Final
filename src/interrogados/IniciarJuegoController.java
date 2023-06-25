cb437659e85315abba059be62da8579d15661ec6
Johan PC <Johan PC@JOHAN>
2023-06-25 17:07:18 -0500

    vr. 1.2 juego interrogados

---
+++ src/interrogados/IniciarJuegoController.java
@@ -0,0 +1,218 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package interrogados;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FXML Controller class
+ *
+ * @author yengal,Ever,Johan
+ */
+public class IniciarJuegoController implements Initializable {
+
+    private int numero_pregunta = -1;
+    private int numero_rapuesta_correcta;
+    private boolean juego_terminado = false;
+    
+    // Salto de linea \n
+    private String[][] preguntas = {
+        {
+            "¿Cuántas veces puede restarse el número 1 del número 1.111?",  // Pregunta
+            "1,111",    // Respuesta 0
+            "1,110",    // Respuesta 1
+            "0",        // Respuesta 2
+            "1",        // Respuesta 3
+            "3",        // Numero respuesta correcta inicia desde 0
+        },
+        {
+            "¿Quien escribio Romeo y julieta?",
+            "Bram Stoker",
+            "William Shakespeare",
+            "Dante Alighieri",
+            "Ninguna de las anteriores",
+            "1",
+        },
+        {
+            "¿Quién es el máximo goleador de la historia del fútbol?",
+            "Messi",
+            "Falcao",
+            "James",
+            "Cristiano",
+            "3",
+        },
+        {
+            "¿En qué año murió Freddie Mercury?",
+            "1991",
+            "1989",
+            "1899",
+            "1992",
+            "0",
+        },
+	{
+            "¿Cuántos huesos hay en el cuerpo humano?",
+            "365",
+            "202",
+            "206",
+            "ninguna de las anteriores",
+            "2",
+         },
+	{
+            "¿Cuánto vale el número pi?",
+            "3.15",
+            "3.1416",
+            "3",
+            "ninguna de las anteriores",
+            "1",
+         },
+        {
+            "Color primario",
+            "blanco",
+            "negro",
+            "azul",
+            "ninguna de las anteriores",
+            "2",
+         },
+	{
+            "¿En qué lugar se da primero el día jueves que el miércoles?",
+            "Monte carlo",
+            "Bogota",
+            "España",
+            "En el diccionario.",
+            "3",
+         },
+        {
+            "Si 5 máquinas hacen 5 artículos en 5 minutos, ¿cuánto tiempo "
+            +"\ndedicarán 100 máquinas en hacer 100 artículos??",
+            "5 minutos",
+            "5 articulos",
+            "5 maquinas",
+            "ninguna de las anteriores",
+            "0",
+         },
+    };
+
+
+    @FXML
+    private Label idLabelPregunta;
+    @FXML
+    private RadioButton radioButtonPregunta1;
+    @FXML
+    private RadioButton radioButtonPregunta2;
+    @FXML
+    private RadioButton radioButtonPregunta3;
+    @FXML
+    private RadioButton radioButtonPregunta4;
+    ToggleGroup toggleGroup = new ToggleGroup();
+    List<RadioButton> radioButtons = new ArrayList<>();
+    @FXML
+    private Button btnMeLaJuego;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Agregamos los radioButtons a un ToggleGroup
+        // para que al seleccionar un radioButton se
+        // desselecciones los demas
+        radioButtonPregunta1.setToggleGroup(this.toggleGroup);
+        radioButtonPregunta2.setToggleGroup(this.toggleGroup);
+        radioButtonPregunta3.setToggleGroup(this.toggleGroup);
+        radioButtonPregunta4.setToggleGroup(this.toggleGroup);
+
+        // Agregamos los radioButtons a una lista
+        // para saber la posicion del radioButton seleccionado
+        this.radioButtons.add(radioButtonPregunta1);
+        this.radioButtons.add(radioButtonPregunta2);
+        this.radioButtons.add(radioButtonPregunta3);
+        this.radioButtons.add(radioButtonPregunta4);
+
+        this.mostrarSiguientePregunta();
+    }
+
+
+    @FXML
+    private void clck(ActionEvent event) {
+        if (!this.juego_terminado) {
+            RadioButton radioButtonSeleccionado = (RadioButton) this.toggleGroup.getSelectedToggle();
+            if (radioButtonSeleccionado != null) {
+                int posicion_seleccionado = radioButtons.indexOf(radioButtonSeleccionado);
+                boolean respuesta_correcta = posicion_seleccionado == this.numero_rapuesta_correcta;
+
+                if (respuesta_correcta) {
+                    this.mostrarSiguientePregunta();
+                } else {
+                    this.finDelJuego();
+                }
+            } else {
+                System.out.println("Ningún RadioButton seleccionado.");
+            }
+        } else {
+            this.reiniciarJuego();
+        }
+    }
+    
+    @FXML
+    private void mostrarSiguientePregunta() {
+        this.numero_pregunta++;
+        String pregunta_actual[] = preguntas[this.numero_pregunta];
+
+        idLabelPregunta.setText(pregunta_actual[0]);       // Pregunta
+        radioButtonPregunta1.setText(pregunta_actual[1]);  // Respuesta 1
+        radioButtonPregunta2.setText(pregunta_actual[2]);  // Respuesta 2
+        radioButtonPregunta3.setText(pregunta_actual[3]);  // Respuesta 3
+        radioButtonPregunta4.setText(pregunta_actual[4]);  // Respuesta 4
+        numero_rapuesta_correcta = Integer.parseInt(pregunta_actual[5]); // Respuesta 5
+
+        // Quitar
+        /*
+        if (this.numero_pregunta == 2) {
+            this.numero_pregunta = -1;
+        }
+        */
+    }
+
+    
+    private void finDelJuego() {
+        this.juego_terminado = true;
+
+        idLabelPregunta.setText("Respuesta incorrecta, fin del juego");
+        radioButtonPregunta1.setVisible(false);
+        radioButtonPregunta2.setVisible(false);
+        radioButtonPregunta3.setVisible(false);
+        radioButtonPregunta4.setVisible(false);
+
+        btnMeLaJuego.setText("Volver a jugar");
+    }
+
+    @FXML
+    private void reiniciarJuego() {
+        this.numero_pregunta = -1;
+        this.juego_terminado = false;
+
+        radioButtonPregunta1.setVisible(true);
+        radioButtonPregunta2.setVisible(true);
+        radioButtonPregunta3.setVisible(true);
+        radioButtonPregunta4.setVisible(true);
+
+        btnMeLaJuego.setText("Me la juego");
+        
+        this.mostrarSiguientePregunta();
+    }
+
+}
---
+++ src/interrogados/Juego.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package interrogados;
+
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Juego extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("IniciarJuego.fxml"));
+            Pane ventana = (Pane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(ventana);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+}
+}
